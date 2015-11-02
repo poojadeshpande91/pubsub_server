8421978445a5e8380503bc2dfc8fa505d5237b05
Tarun Arora <tananrules@gmail.com>
2015-11-02 18:35:24 +0530

    Publish i1

--- app.js
+++ app.js
@@ -18,13 +18,14 @@ var client = new nodeXmppClient.Client({
 console.log('Create Node with Name - 1');
 console.log('Create Node without Name - 2');
 console.log('Discover all Nodes - 3');
+console.log('Delete Node - 4');
+console.log('Publish to a Node - 5');
 console.log('Enter Appropiate Choice');
 
 
 var pubsubClient = client.pubsub('pubsub.roidsoftware.com');
 
 prompt.get('choice', function(err, result) {
-  debugger
   switch(parseInt(result.choice)) {
     case 1:
       prompt.get('nodeName', function(err, result) {
@@ -47,38 +48,29 @@ prompt.get('choice', function(err, result) {
       });
     break;
 
+    case 4:
+      prompt.get('nodeName', function(err, result) {
+        console.log('nodeName: ' + result.nodeName);
+        pubsubClient.deleteNode(result.nodeName, function() {
+          console.log('Node Deleted');
+        });
+      });
+    break;
+
+    case 5:
+      var node,
+          content;
+
+      prompt.get(['node', 'content'], function(err, result) {
+        node = result.node;
+        content = result.content;
+        debugger
+        pubsubClient.publish(node, content);
+      });
+    break;
+
     default:
       console.log('Wrong choice or input');
     break;
   }
 });
-
-
-// client.on('online', function() {
-//     console.log('I m online');
-//     var pubsubClient = client.pubsub('pubsub.roidsoftware.com');
-
-//     prompt.get('nodeName', function(err, result) {
-//       console.log('  nodeName: ' + result.nodeName);
-//       pubsubClient.createNodeWithName(result.nodeName, function() {
-//         console.log('created');
-//       });
-//     });
-
-// });
-
-// client.on('online', function() {
-//     console.log('I m online');
-//     var pubsubClient = client.pubsub('pubsub.roidsoftware.com');
-//     pubsubClient.createNodeWithoutName(function() {
-//       console.log('created');
-//   });
-// });
-
-// client.on('online', function() {
-//   console.log('I m online');
-//   var pubsubClient = client.pubsub('pubsub.roidsoftware.com');
-//   pubsubClient.discoNodes(function(){
-//     console.log('discovery nodes callback');
-//   });
-// });
